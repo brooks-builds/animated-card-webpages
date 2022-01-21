1902bcf9e2d7da111c04383cb783b5a6b5c05903
Tony Johnson <tony@tosp.net.au>
2022-01-21 13:50:14 +1100

    Refactor App to be a function component

--- christmas_2021/src/lib.rs
+++ christmas_2021/src/lib.rs
@@ -5,26 +5,13 @@ use routes::{switch, Route};
 use yew::prelude::*;
 use yew_router::prelude::*;
 
-pub struct App;
-
-impl Component for App {
-    type Message = ();
-
-    type Properties = ();
-
-    fn create(_ctx: &Context<Self>) -> Self {
-        // get the link information here and store it
-        Self
-    }
-
-    fn view(&self, _ctx: &Context<Self>) -> Html {
-        // pass in the router information via a property
-        html! {
-            <BrowserRouter>
-                <main>
-                    <Switch<Route> render={Switch::render(switch)} />
-                </main>
-            </BrowserRouter>
-        }
+#[function_component(App)]
+pub fn app() -> Html {
+    html! {
+        <BrowserRouter>
+            <main>
+                <Switch<Route> render={Switch::render(switch)} />
+            </main>
+        </BrowserRouter>
     }
 }
